78492747e6303c876e3f27c0a0527c25c9bebba9
longye <longye@tom.com>
2020-04-01 19:25:30 +0800

    person类增加了name属性

--- src/main/java/com/newer/config/person.java
+++ src/main/java/com/newer/config/person.java
@@ -1,5 +1,14 @@
 package com.newer.config;
 
 public class person {
+ private String name;
 
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+ 
 }
