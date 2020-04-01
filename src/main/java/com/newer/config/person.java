33bcd6a80cab9ddcb5601e18087b6636774f64f3
longye <longye@tom.com>
2020-04-01 19:42:52 +0800

    person类增加了id属性

--- src/main/java/com/newer/config/person.java
+++ src/main/java/com/newer/config/person.java
@@ -1,5 +1,14 @@
 package com.newer.config;
 
 public class person {
+    private Integer id;
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+    
 }
